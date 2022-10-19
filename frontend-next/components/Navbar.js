a3d805f6a0724398a80b75c4940d0a5cedf86919
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-10-19 17:04:32 -0400

    create home page banner and added some css

--- frontend-next/components/Navbar.js
+++ frontend-next/components/Navbar.js
@@ -81,7 +81,7 @@ function classNames(...classes) {
 export default function Navbar() {
   return (
  
-    <Popover className="relative bg-white">
+    <Popover className="relative sticky top-0 z-50">
       <div className="mx-auto max-w-7xl px-4 sm:px-6">
         <div className="flex items-center justify-between py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
--- frontend-next/pages/index.js
+++ frontend-next/pages/index.js
@@ -1,7 +1,58 @@
-import Link from 'next/link'
+
 
 export default function Home() {
   return (
-    <div>home</div>
+    <div>
+      <main>
+        <div>
+          {/* Hero card */}
+          <div className="relative">
+            <div className="absolute inset-x-0 bottom-0 h-1/2 bg-[#22A2A3] bg-opacity-50" />
+            <div className="mx-auto max-w-7xl sm:px-4 lg:px-6">
+              <div className="relative shadow-xl sm:overflow-hidden ">
+                <div className="absolute inset-0">
+                  <img
+                    className="h-full w-full object-cover"
+                    src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
+                    alt="People working on laptops"
+                  />
+                  <div className="absolute inset-0 bg-white-700 mix-blend-multiply" />
+                </div>
+                <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
+                  <h1 className="text-center text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
+                    <span className="block text-white">Take control of your</span>
+                    <span className="block text-[#22A2A3]">customer support</span>
+                  </h1>
+                  <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
+                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
+                    amet fugiat veniam occaecat fugiat aliqua.
+                  </p>
+                  <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
+                    <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+                      <a
+                        href="#"
+                        className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-[#22A2A3] shadow-sm hover:bg-indigo-50 sm:px-8"
+                      >
+                        Get started
+                      </a>
+                      <a
+                        href="#"
+                        className="flex items-center justify-center rounded-md border border-transparent bg-[#22A2A3] bg-opacity-60 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-opacity-70 sm:px-8"
+                      >
+                        Live demo
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          
+        </div>
+
+        {/* More main page content here... */}
+      </main>
+    </div>
   )
 }

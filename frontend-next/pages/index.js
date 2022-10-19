e4cab7d1e6647d448d3d8cd3a60539b1f0984b37
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-10-19 14:36:37 -0400

    create new navbar with freeone logo and added some css

--- frontend-next/pages/index.js
+++ frontend-next/pages/index.js
@@ -2,27 +2,6 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/about">
-          <a>About us</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/contact">
-          <a>Contact us</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/services">
-          <a>Services</a>
-        </Link>
-      </li>
-    </ul>
+    <div>home</div>
   )
 }

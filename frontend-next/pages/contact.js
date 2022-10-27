ff643b38b627a83e163c12027d1e003845915a04
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-10-26 22:48:45 -0400

    modified contact page

--- frontend-next/pages/contact.js
+++ frontend-next/pages/contact.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
+
 
 export default function Contact() {
   const [fullname, setFullname] = useState("");
@@ -81,109 +83,170 @@ export default function Contact() {
   };
 
   return (
-
     <div>
       <div className="relative bg-indigo-800">
+        <div className="absolute inset-0">
+          <img
+            className="h-full w-full object-cover"
+            src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&&sat=-100"
+            alt=""
+          />
+          <div
+            className="absolute inset-0 bg-indigo-800 mix-blend-multiply"
+            aria-hidden="true"
+          />
+        </div>
+        <div className="relative mx-auto max-w-7xl py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
+          <h1 className="text-4xl font-bold tracking-tight text-white sm:text-5xl lg:text-6xl">
+            Get in touch
+          </h1>
+          <p className="mt-6 max-w-3xl text-xl text-indigo-100">
+            Mattis amet hendrerit dolor, quisque lorem pharetra. Pellentesque
+            lacus nisi urna, arcu sociis eu. Orci vel lectus nisl eget eget ut
+            consectetur. Sit justo viverra non adipisicing elit distinctio.
+          </p>
+        </div>
+      </div>
+
+      
+
+
+      <div className="relative bg-white">
       <div className="absolute inset-0">
-        <img
-          className="h-full w-full object-cover"
-          src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&&sat=-100"
-          alt=""
+        <div className="absolute inset-y-0 left-0 w-1/2 bg-gray-50" />
+      </div>
+      <div className="relative mx-auto max-w-7xl lg:grid lg:grid-cols-5">
+        <div className="bg-gray-50 py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
+          <div className="mx-auto max-w-lg">
+            <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">Get in touch</h2>
+            <p className="mt-3 text-lg leading-6 text-gray-500">
+              Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus
+              arcu.
+            </p>
+            <dl className="mt-8 text-base text-gray-500">
+              <div>
+                <dt className="sr-only">Postal address</dt>
+                <dd>
+                  <p>742 Evergreen Terrace</p>
+                  <p>Springfield, OR 12345</p>
+                </dd>
+              </div>
+              <div className="mt-6">
+                <dt className="sr-only">Phone number</dt>
+                <dd className="flex">
+                  <PhoneIcon className="h-6 w-6 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                  <span className="ml-3">+1 (555) 123-4567</span>
+                </dd>
+              </div>
+              <div className="mt-3">
+                <dt className="sr-only">Email</dt>
+                <dd className="flex">
+                  <EnvelopeIcon className="h-6 w-6 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                  <span className="ml-3">support@example.com</span>
+                </dd>
+              </div>
+            </dl>
+            <p className="mt-6 text-base text-gray-500">
+              Looking for careers?{' '}
+              <a href="#" className="font-medium text-gray-700 underline">
+                View all job openings
+              </a>
+              .
+            </p>
+          </div>
+        </div>
+        <div className="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
+          <div className="mx-auto max-w-lg lg:max-w-none">
+          <form
+        onSubmit={handleSubmit}
+        className="form rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white dark:bg-white"
+      >
+        <h1 className="text-2xl font-bold dark:text-[#22A2A3]">
+          Send a message
+        </h1>
+
+        <input
+          type="text"
+          value={fullname}
+          onChange={(e) => {
+            setFullname(e.target.value);
+          }}
+          name="fullname"
+          placeholder="Full name"
+          className="form-input block w-full rounded-md border mt-5 py-2 px-3 placeholder-gray-400  focus:outline-none focus:rounded-md focus:ring-1 ring-green-500"
         />
-        <div className="absolute inset-0 bg-indigo-800 mix-blend-multiply" aria-hidden="true" />
-      </div>
-      <div className="relative mx-auto max-w-7xl py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
-        <h1 className="text-4xl font-bold tracking-tight text-white sm:text-5xl lg:text-6xl">Get in touch</h1>
-        <p className="mt-6 max-w-3xl text-xl text-indigo-100">
-          Mattis amet hendrerit dolor, quisque lorem pharetra. Pellentesque lacus nisi urna, arcu sociis eu. Orci vel
-          lectus nisl eget eget ut consectetur. Sit justo viverra non adipisicing elit distinctio.
-        </p>
+        {errors?.fullname && (
+          <p className="text-red-500">Full name cannot be empty.</p>
+        )}
+
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          placeholder="E-mail"
+          className="form-input block w-full rounded-md border mt-5 py-2 px-3 placeholder-gray-400  focus:outline-none focus:rounded-md focus:ring-1 ring-green-500"
+        />
+        {errors?.email && (
+          <p className="text-red-500">Email cannot be empty.</p>
+        )}
+
+        <input
+          type="text"
+          name="subject"
+          value={subject}
+          onChange={(e) => {
+            setSubject(e.target.value);
+          }}
+          placeholder="Subject"
+          className="form-input block w-full rounded-md border mt-5 py-2 px-3 placeholder-gray-400  focus:outline-none focus:rounded-md focus:ring-1 ring-green-500"
+        />
+        {errors?.subject && (
+          <p className="text-red-500">Subject cannot be empty.</p>
+        )}
+
+        <textarea
+          name="message"
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+          placeholder="Message"
+          className="form-input block w-full rounded-md border mt-5 py-2 px-3 placeholder-gray-400  focus:outline-none focus:rounded-md focus:ring-1 ring-green-500"
+        ></textarea>
+        {errors?.message && (
+          <p className="text-red-500">Message body cannot be empty.</p>
+        )}
+        <div className="flex flex-row items-center justify-start">
+          <button
+            type="submit"
+            className="text-white bg-[#E60D51] hover:bg-[#22A2A3] focus:outline-none focus:ring-1 focus:ring-[#E60D51] font-medium rounded-md text-sm mt-5 px-5 py-2.5 text-center mb-2 dark:bg-[#E60D51] dark:hover:bg-[#22A2A3] dark:focus:ring-[#E60D51]"
+          >
+            {buttonText}
+          </button>
+        </div>
+        <div className="text-left">
+          {showSuccessMessage && (
+            <p className="text-green-500 font-semibold text-sm my-2">
+              Thankyou! Your Message has been delivered.
+            </p>
+          )}
+          {showFailureMessage && (
+            <p className="text-red-500">
+              Oops! Something went wrong, please try again.
+            </p>
+          )}
+        </div>
+      </form>
+          </div>
+        </div>
       </div>
     </div>
 
 
-    <form
-      onSubmit={handleSubmit}
-      className="form rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white dark:bg-white"
-    >
-      <h1 className="text-2xl font-bold dark:text-[#22A2A3]">Send a message</h1>
-
-      <input
-        type="text"
-        value={fullname}
-        onChange={(e) => {
-          setFullname(e.target.value);
-        }}
-        name="fullname"
-        placeholder="Full name"
-        className="form-input block w-full rounded-md border mt-5 py-2 px-3 placeholder-gray-400  focus:outline-none focus:rounded-md focus:ring-1 ring-green-500"
-      />
-      {errors?.fullname && (
-        <p className="text-red-500">Full name cannot be empty.</p>
-      )}
-
-      <input
-        type="email"
-        name="email"
-        value={email}
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-        placeholder="E-mail"
-        className="form-input block w-full rounded-md border mt-5 py-2 px-3 placeholder-gray-400  focus:outline-none focus:rounded-md focus:ring-1 ring-green-500"
-      />
-      {errors?.email && <p className="text-red-500">Email cannot be empty.</p>}
-
-      <input
-        type="text"
-        name="subject"
-        value={subject}
-        onChange={(e) => {
-          setSubject(e.target.value);
-        }}
-        placeholder="Subject"
-        className="form-input block w-full rounded-md border mt-5 py-2 px-3 placeholder-gray-400  focus:outline-none focus:rounded-md focus:ring-1 ring-green-500"
-      />
-      {errors?.subject && (
-        <p className="text-red-500">Subject cannot be empty.</p>
-      )}
-
-      <textarea
-        name="message"
-        value={message}
-        onChange={(e) => {
-          setMessage(e.target.value);
-        }}
-        placeholder="Message"
-        className="form-input block w-full rounded-md border mt-5 py-2 px-3 placeholder-gray-400  focus:outline-none focus:rounded-md focus:ring-1 ring-green-500"
-      ></textarea>
-      {errors?.message && (
-        <p className="text-red-500">Message body cannot be empty.</p>
-      )}
-      <div className="flex flex-row items-center justify-start">
-        <button
-          type="submit"
-          className="text-white bg-[#E60D51] hover:bg-[#22A2A3] focus:outline-none focus:ring-1 focus:ring-[#E60D51] font-medium rounded-md text-sm mt-5 px-5 py-2.5 text-center mb-2 dark:bg-[#E60D51] dark:hover:bg-[#22A2A3] dark:focus:ring-[#E60D51]"
-        >
-          {buttonText}
-        </button>
-      </div>
-      <div className="text-left">
-        {showSuccessMessage && (
-          <p className="text-green-500 font-semibold text-sm my-2">
-            Thankyou! Your Message has been delivered.
-          </p>
-        )}
-        {showFailureMessage && (
-          <p className="text-red-500">
-            Oops! Something went wrong, please try again.
-          </p>
-        )}
-      </div>
-    </form>
 
     </div>
-    
-    
   );
 }

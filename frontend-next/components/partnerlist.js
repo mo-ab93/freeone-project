5f126abc604e4d90bd2e26c074383a36b38a2404
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-10-28 20:25:37 -0400

    create new component for partners list

---
+++ frontend-next/components/partnerlist.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Partnerlist({ title, slug, logo }) {
+  return (
+    <Link href={`partner/${slug}`} >
+      <li>
+        <div className="space-y-4 shadow rounded">
+          <div className="aspect-w-4 aspect-h-2">
+            <img className=" object-cover rounded-t	" src={logo} alt="" />
+          </div>
+
+          <div className="space-y-2 px-3 py-2">
+            <div>
+              <h3 className="text-lg font-medium tracking-tight text-gray-900">
+                {title}
+              </h3>
+            </div>
+          </div>
+        </div>
+      </li>
+    </Link>
+  );
+}

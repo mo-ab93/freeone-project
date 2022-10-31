0dd7cc5611abd02db6dcbed7de95da75258e1d97
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-10-31 17:03:46 -0400

    add contact form to home page

--- frontend-next/pages/index.js
+++ frontend-next/pages/index.js
@@ -1,5 +1,6 @@
 import Featureslist from "../components/featureslist";
 import { useState } from "react";
+import Form from "../components/form";
 
 export default function Home({ stuff }) {
   const [features, setfeatures] = useState(stuff);
@@ -132,6 +133,60 @@ export default function Home({ stuff }) {
           </div>
         </div>
       </div>
+      <div className="overflow-hidden bg-white py-16 px-4 sm:px-6 lg:px-8 lg:py-24">
+      <div className="relative mx-auto max-w-xl">
+        <svg
+          className="absolute left-full translate-x-1/2 transform"
+          width={404}
+          height={404}
+          fill="none"
+          viewBox="0 0 404 404"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="85737c0e-0916-41d7-917f-596dc7edfa27"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
+        </svg>
+        <svg
+          className="absolute right-full bottom-0 -translate-x-1/2 transform"
+          width={404}
+          height={404}
+          fill="none"
+          viewBox="0 0 404 404"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="85737c0e-0916-41d7-917f-596dc7edfa27"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
+        </svg>
+        <div className="text-center">
+        </div>
+        <div className="mt-12">
+        <Form/>
+        </div>
+      </div>
+    </div>
+      
     </div>
   );
 }
@@ -145,3 +200,11 @@ export async function getStaticProps() {
     props: { stuff },
   };
 }
+
+
+
+
+
+
+   
+

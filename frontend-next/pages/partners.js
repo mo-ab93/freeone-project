909f0b68e4ae9f02a727577e217d38c6eb9887eb
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-11-01 13:29:05 -0400

    add padding top and bottom

--- frontend-next/pages/partners.js
+++ frontend-next/pages/partners.js
@@ -5,7 +5,7 @@ import { fetchAPI } from "../lib/api";
 
 export default function Partners({ partners }) {
   return (
-    <div className="bg-slate-50">
+    <div className="bg-slate-50 py-4">
       <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:px-8 lg:py-3">
         <div className="space-y-12">
         <ul className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">

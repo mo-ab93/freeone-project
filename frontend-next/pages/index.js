e6841d36f27db10653ad0c9c003a68329e60dfe0
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-11-02 18:36:56 -0400

    updated getStaticProps function and populate data

--- frontend-next/pages/index.js
+++ frontend-next/pages/index.js
@@ -1,9 +1,8 @@
 import Featureslist from "../components/featureslist";
-import { useState } from "react";
 import Form from "../components/form";
+import { fetchAPI } from "../lib/api";
 
-export default function Home({ stuff }) {
-  const [features, setfeatures] = useState(stuff);
+export default function Home({ featureStuff, contectStuff }) {
   return (
     <div>
       <main>
@@ -15,7 +14,7 @@ export default function Home({ stuff }) {
                 <div className="absolute inset-0">
                   <img
                     className="h-full w-full object-cover"
-                    src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
+                    src={contectStuff.attributes.heroimg.data.attributes.url}
                     alt="People working on laptops"
                   />
                   <div className="absolute inset-0 bg-white-700 mix-blend-multiply" />
@@ -23,30 +22,28 @@ export default function Home({ stuff }) {
                 <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
                   <h1 className="text-center text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
                     <span className="block text-white">
-                      Take control of your
+                      {contectStuff.attributes.h1span1}
                     </span>
                     <span className="block text-[#22A2A3]">
-                      customer support
+                      {contectStuff.attributes.h1span2}
                     </span>
                   </h1>
                   <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
-                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                    qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
-                    occaecat fugiat aliqua.
+                    {contectStuff.attributes.description}
                   </p>
                   <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
                     <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
                       <a
-                        href="#"
+                        href={contectStuff.attributes.button1link}
                         className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-[#22A2A3] shadow-sm hover:bg-indigo-50 sm:px-8"
                       >
-                        Get started
+                        {contectStuff.attributes.button1}
                       </a>
                       <a
-                        href="#"
+                        href={contectStuff.attributes.button2link}
                         className="flex items-center justify-center rounded-md border border-transparent bg-[#22A2A3] bg-opacity-60 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-opacity-70 sm:px-8"
                       >
-                        Live demo
+                        {contectStuff.attributes.button2}
                       </a>
                     </div>
                   </div>
@@ -58,29 +55,31 @@ export default function Home({ stuff }) {
 
         {/* More main page content here... */}
       </main>
-      <h1 className="text-xl font-semibold text-[#22A2A3] text-center py-14">Services & Solutions</h1>
-          <div className="bg-white">
-            <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:px-8 lg:py-3">
-            <div className="space-y-12">
+      <h1 className="text-xl font-semibold text-[#22A2A3] text-center py-14">
+        {contectStuff.attributes.featuretitle}
+      </h1>
+      <div className="bg-white">
+        <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:px-8 lg:py-3">
+          <div className="space-y-12">
             <ul
-            role="list"
-            className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8"
-          >
-      {features &&
-        features.data.map((detail) => (
-              <Featureslist
-                key={detail.id}
-                imageUrl={
-                  detail.attributes.image.data.attributes.formats.medium.url
-                }
-                name={detail.attributes.name}
-                description={detail.attributes.description}
-              />
-              ))}
-              </ul>
-              </div>
-              </div>
-            </div>
+              role="list"
+              className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8"
+            >
+              {featureStuff &&
+                featureStuff.data.map((detail) => (
+                  <Featureslist
+                    key={detail.id}
+                    imageUrl={
+                      detail.attributes.image.data.attributes.formats.medium.url
+                    }
+                    name={detail.attributes.name}
+                    description={detail.attributes.description}
+                  />
+                ))}
+            </ul>
+          </div>
+        </div>
+      </div>
 
       <div className="bg-white">
         <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
@@ -134,79 +133,98 @@ export default function Home({ stuff }) {
         </div>
       </div>
       <div className="overflow-hidden bg-white py-16 px-4 sm:px-6 lg:px-8 lg:py-16">
-      <div className="relative mx-auto max-w-2xl">
-        <svg
-          className="absolute left-full translate-x-1/2 transform"
-          width={404}
-          height={404}
-          fill="none"
-          viewBox="0 0 404 404"
-          aria-hidden="true"
-        >
-          <defs>
-            <pattern
-              id="85737c0e-0916-41d7-917f-596dc7edfa27"
-              x={0}
-              y={0}
-              width={20}
-              height={20}
-              patternUnits="userSpaceOnUse"
-            >
-              <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-            </pattern>
-          </defs>
-          <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
-        </svg>
-        <svg
-          className="absolute right-full bottom-0 -translate-x-1/2 transform"
-          width={404}
-          height={404}
-          fill="none"
-          viewBox="0 0 404 404"
-          aria-hidden="true"
-        >
-          <defs>
-            <pattern
-              id="85737c0e-0916-41d7-917f-596dc7edfa27"
-              x={0}
-              y={0}
-              width={20}
-              height={20}
-              patternUnits="userSpaceOnUse"
-            >
-              <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-            </pattern>
-          </defs>
-          <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
-        </svg>
-        <div className="text-center">
-          <h2>Contact us</h2>
-          <p>Stay in Toch with us</p>
-        </div>
-        <div className="mt-4">
-        <Form/>
+        <div className="relative mx-auto max-w-2xl">
+          <svg
+            className="absolute left-full translate-x-1/2 transform"
+            width={404}
+            height={404}
+            fill="none"
+            viewBox="0 0 404 404"
+            aria-hidden="true"
+          >
+            <defs>
+              <pattern
+                id="85737c0e-0916-41d7-917f-596dc7edfa27"
+                x={0}
+                y={0}
+                width={20}
+                height={20}
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x={0}
+                  y={0}
+                  width={4}
+                  height={4}
+                  className="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width={404}
+              height={404}
+              fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+            />
+          </svg>
+          <svg
+            className="absolute right-full bottom-0 -translate-x-1/2 transform"
+            width={404}
+            height={404}
+            fill="none"
+            viewBox="0 0 404 404"
+            aria-hidden="true"
+          >
+            <defs>
+              <pattern
+                id="85737c0e-0916-41d7-917f-596dc7edfa27"
+                x={0}
+                y={0}
+                width={20}
+                height={20}
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x={0}
+                  y={0}
+                  width={4}
+                  height={4}
+                  className="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width={404}
+              height={404}
+              fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+            />
+          </svg>
+          <div className="text-center">
+            <h2>Contact us</h2>
+            <p>Stay in Toch with us</p>
+          </div>
+          <div>
+            <Form />
+          </div>
         </div>
       </div>
-    </div>
-      
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const results = await fetch(
-    "http://localhost:1337/api/featureslists?populate=*"
-  );
-  const stuff = await results.json();
-  return {
-    props: { stuff },
-  };
+  try {
+    const featureStuff = await fetchAPI("/featureslists?populate=*");
+    const contectStuff = await fetchAPI("/homepage?populate=*");
+    const responses = await Promise.all([featureStuff, contectStuff]);
+    return {
+      props: {
+        featureStuff: responses[0],
+        contectStuff: responses[1].data,
+      },
+    };
+  } catch (error) {
+    return { error };
+  }
 }
-
-
-
-
-
-
-   
-

f8e0f390b8ec91fdd85454ae4d09816b196f5d4b
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-11-10 18:25:46 -0500

    create new page for privacy policy

--- frontend-next/pages/privacy_policy.js
+++ frontend-next/pages/privacy_policy.js
@@ -1,6 +1,156 @@
+import { fetchAPI } from "../lib/api";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
 
-export default function Privacy_policy() {
+export default function Privacy_policy({ content }) {
   return (
-    <div><h1>Privacy policy</h1></div>
-  )
+    <div>
+      <div className="relative overflow-hidden bg-white py-16">
+        <div className="hidden lg:absolute lg:inset-y-0 lg:block lg:h-full lg:w-full lg:[overflow-anchor:none]">
+          <div
+            className="relative mx-auto h-full max-w-prose text-lg"
+            aria-hidden="true"
+          >
+            <svg
+              className="absolute top-12 left-full translate-x-32 transform"
+              width={404}
+              height={384}
+              fill="none"
+              viewBox="0 0 404 384"
+            >
+              <defs>
+                <pattern
+                  id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={384}
+                fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
+              />
+            </svg>
+            <svg
+              className="absolute top-1/2 right-full -translate-y-1/2 -translate-x-32 transform"
+              width={404}
+              height={384}
+              fill="none"
+              viewBox="0 0 404 384"
+            >
+              <defs>
+                <pattern
+                  id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={384}
+                fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+              />
+            </svg>
+            <svg
+              className="absolute bottom-12 left-full translate-x-32 transform"
+              width={404}
+              height={384}
+              fill="none"
+              viewBox="0 0 404 384"
+            >
+              <defs>
+                <pattern
+                  id="d3eb07ae-5182-43e6-857d-35c643af9034"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={384}
+                fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="relative px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-prose text-lg">
+            <h1>
+              <span className="block text-center text-lg font-semibold text-[#22A2A3]">
+                {content.data.attributes.companyname}
+              </span>
+              <span className="mt-2 block text-center text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+                {content.data.attributes.pagetitle}
+              </span>
+            </h1>
+            <p className="mt-8 text-xl leading-8 text-gray-500">
+              {content.data.attributes.paragraph1}
+            </p>
+          </div>
+          <div className="prose prose-lg prose-indigo mx-auto mt-6 text-gray-500">
+            <p>{content.data.attributes.paragraph2}</p>
+
+            <p>{content.data.attributes.paragraph3}</p>
+            <h2>{content.data.attributes.heading1}</h2>
+            <p>{content.data.attributes.paragraph4}</p>
+
+            <p>{content.data.attributes.paragraph5}</p>
+
+            <h2>{content.data.attributes.heading2}</h2>
+            <p>{content.data.attributes.paragraph6}</p>
+            <p>{content.data.attributes.paragraph7}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  try {
+    const content = await fetchAPI("/privacy-policy?populate=*");
+    return {
+      props: {
+        content,
+      },
+    };
+  } catch (error) {
+    return { error };
+  }
 }
